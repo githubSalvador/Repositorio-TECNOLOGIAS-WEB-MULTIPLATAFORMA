14abb0b3a24945f2987cf962fa3ebb248de2da79
DESKTOP-1LH04QV\HP <lic.salvador01@gmail.com>
2018-03-21 23:44:23 -0600

    dfdf

---
+++ Practica_Componentes_Reacstrap_/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Principal from './componentes/principal';
+
+const app=document.getElementById('app');
+
+
+ReactDOM.render(<Principal/>,app);
